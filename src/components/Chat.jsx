7c9ce8e7c1dbbf3d7a44121b44709b5bce065635
Vadim <savelyevresearch@protonmail.com>
2022-11-05 18:00:52 +0600

    The Chat component received a header with information

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+
+import VideoStreamingIcon from '../assets/videoStreamingIcon.png';
+import AddUserIcon from '../assets/addUserIcon.png';
+import MoreIcon from '../assets/moreIcon.png';
 
 const Chat = () => {
   return (
     <div className='chat'>
-        Chat
+        <div className='chatInfo'>
+            <span>Jane</span>
+            <div className='chatIcons'>
+                <img src={VideoStreamingIcon} alt=''/>
+                <img src={AddUserIcon} alt=''/>
+                <img src={MoreIcon} alt=''/>
+            </div>
+        </div>
     </div>
   )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,9 +7,9 @@ const Search = () => {
             <input type='text' placeholder='Find a user'/>
         </div>
         <div className='userChat'>
-            <img src='https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' alt=''/>
+            <img src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80' alt=''/>
             <div className='userChatInfo'>
-                <span>Jane</span>
+                <span>John</span>
             </div>
         </div>
     </div>
