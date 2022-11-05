9e39c68d49f72b6feb0b51c89457e43725bcde49
Vadim <savelyevresearch@protonmail.com>
2022-11-05 17:13:14 +0600

    The Search component has been designed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+        <div className='searchForm'>
+            <input type='text' placeholder='find a user'/>
+        </div>
+        <div className='userChat'>
+            <img src='https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' alt=''/>
+            <div className='userChatInfo'>
+                <span>Jane</span>
+            </div>
+        </div>
+    </div>
   )
 }
 
