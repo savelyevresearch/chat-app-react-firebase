7b21678b32c88ed655dcebcf47ec9c94a04ac239
Vadim <savelyevresearch@protonmail.com>
2022-11-06 14:54:12 +0600

    The functionality of the Search component has been integrated

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import { collection, getDocs, query, where } from 'firebase/firestore';
+
+import { db } from '../firebase';
 
 const Search = () => {
+  const [username, setUsername] = useState('');
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(false);
+
+  const handleSearch = async () => {
+    const queryResult = query(
+      collection(db, 'users'),
+      where('displayName', '==', username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(queryResult);
+
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  const handleKey = (event) => {
+    event.code === 'Enter' && handleSearch();
+  };
+
   return (
     <div className='search'>
         <div className='searchForm'>
-            <input type='text' placeholder='Find a user'/>
+            <input
+              type='text'
+              placeholder='Find a user'
+              onKeyDown={handleKey}
+              onChange={
+                (event) => setUsername(event.target.value)
+              }  
+            />
         </div>
-        <div className='userChat'>
-            <img src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80' alt=''/>
+        {error && <span>User not found!</span>}
+        {user && <div className='userChat'>
+            <img src={user.photoURL} alt=''/>
             <div className='userChatInfo'>
-                <span>John</span>
+                <span>{user.displayName}</span>
             </div>
-        </div>
+        </div>}
     </div>
   )
 }
